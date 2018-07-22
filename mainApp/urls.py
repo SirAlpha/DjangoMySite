cdbdd203a466ed12863aadf56ad7ce302c6a48e4
Nick Terskikh <sir.pc-help@yandex.ru>
2018-07-22 20:19:13 +0800

    общий коммит

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -13,14 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+
 from django.conf.urls import url
-from django.urls import include, path
+from django.urls import include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('webexample/', include('webexample.urls')),
-    url(r'^$', include('mainApp.urls')),
-    url(r'^webexample/', include('webexample.urls')),
+    url(r'^$', views.index, name='index'),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -21,5 +21,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('webexample/', include('webexample.urls')),
+    url(r'^$', include('mainApp.urls')),
     url(r'^webexample/', include('webexample.urls')),
 ]
